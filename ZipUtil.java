8492a16b41cbfbc19e6f8067934e01c5ac7d9d53
Avalon <avalon451990@users.noreply.github.com>
2017-02-09 11:15:31 +0800

    提交
    完善解压缩

--- ZipUtil.java
+++ ZipUtil.java
@@ -1,9 +1,23 @@
+package com.pt.theme;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
+import javax.annotation.processing.FilerException;
+
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.taskdefs.Zip;
+import org.apache.tools.ant.types.FileSet;
 
 /**
  * Created on 16/6/28.
@@ -26,18 +40,21 @@ public class ZipUtil {
     public static void UnZipFile(ZipInputStream inZip, String outPutPath) throws Exception{
         File file = new File(outPutPath);
         if (!file.exists()) {
-            file.mkdirs();
+        	file.mkdirs();
         }
         ZipEntry zipEntry;
         String szName = "";
-        while ((zipEntry = inZip.getNextEntry()) != null){
+        while ((zipEntry = inZip.getNextEntry()) != null && !zipEntry.isDirectory()){
             szName = zipEntry.getName();
             if (zipEntry.isDirectory()){
                 szName = szName.substring(0, szName.length() - 1);
                 File folder = new File(outPutPath + File.separator + szName);
                 folder.mkdirs();
             }else{
-                file = new File(outPutPath + File.separator + szName);
+            	file = new File(outPutPath + File.separator + szName);
+                if (!file.getParentFile().exists()) {
+                	file.getParentFile().mkdirs();
+				}
                 file.createNewFile();
                 OutputStream outStream = new FileOutputStream(file);
                 byte[] buffer = new byte[1024];
@@ -51,5 +68,71 @@ public class ZipUtil {
         }
         inZip.close();
     }
+    
+    /** 
+     * 将存放在sourceFilePath目录下的源文件，打包成fileName名称的zip文件，并存放到zipFilePath路径下 
+     * @param sourceFilePath :待压缩的文件路径 
+     * @param zipFilePath :压缩后存放路径 
+     * @param fileName :压缩后文件的名称 
+     * @throws FilerException 
+     */  
+    public static void fileToZip(String sourceFilePath, String zipFilePath) throws FilerException {
+    	Project project = new Project();
+    	Zip zip = new Zip();
+    	zip.setProject(project);
+    	zip.setDestFile(new File(zipFilePath));
+    	FileSet fileSet = new FileSet();
+    	fileSet.setProject(project);
+    	fileSet.setDir(new File(sourceFilePath));
+    	zip.addFileset(fileSet);
+    	zip.execute();
+//    	FileOutputStream outputStream = null;
+//    	ZipOutputStream zipOutputStream = null;
+//    	try {
+//    		outputStream = new FileOutputStream(zipFilePath);
+//    		zipOutputStream = new ZipOutputStream(outputStream);
+//    		fileToZip(new File(sourceFilePath), zipOutputStream, "");
+//		} catch (Exception e) {
+//			throw new FilerException("创建压缩文件失败！");
+//		}finally {
+//			try {
+//				if (zipOutputStream!=null) {
+//					zipOutputStream.close();
+//				}
+//				if (outputStream!=null) {
+//					outputStream.close();
+//				}
+//			} catch (Exception e2) {
+//			}
+//		}
+	}
+    
+//    public static void fileToZip(File sourceFile,ZipOutputStream zipFile, String pathPrefix) throws Exception{  
+//    	if (!sourceFile.exists()) {
+//			throw new FileNotFoundException(sourceFile.getAbsolutePath() + "not found!");
+//		}
+//    	String[] filenames = sourceFile.list();
+//    	for (int i = 0; i < filenames.length; i++) {
+//			String filename = filenames[i];
+//			File file = new File(sourceFile, filename);
+//			String path = pathPrefix + file.getName();
+//			if (file.isDirectory()) {
+//				path += "/";
+//				fileToZip(file, zipFile, path);
+//			}else{
+//				ZipEntry zipEntry = new ZipEntry(path);  
+//				zipFile.putNextEntry(zipEntry);
+//				FileInputStream inputStream = new FileInputStream(file);
+//				BufferedInputStream bis = new BufferedInputStream(inputStream, 1024);
+//				byte[] bufs = new byte[1024];
+//				int read = 0;  
+//                while((read=bis.read(bufs, 0, 1024)) != -1){  
+//                	zipFile.write(bufs,0,read);  
+//                }
+//                bis.close();
+//                inputStream.close();
+//			}
+//		}
+//    }  
 
 }
